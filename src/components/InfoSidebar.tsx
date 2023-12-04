afd68c48c10611a4e245220ea978121251d665fb
clintonlunn <cpl09c@gmail.com>
2023-12-04 15:07:36 -0700

    adding more sidebar placeholders, update some colors

---
+++ src/components/InfoSidebar.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import {
+  CalcitePanel,
+  CalciteBlock,
+  CalciteButton,
+  CalciteLink,
+  CalciteModal,
+} from '@esri/calcite-components-react'
+
+interface InfoSidebarProps {
+  setPanelClosed: React.Dispatch<React.SetStateAction<boolean>>
+  setShellPanelCollapsed: React.Dispatch<React.SetStateAction<boolean>>
+  panelHeading: string
+  panelClosed: boolean
+}
+
+function InfoSidebar({
+  setPanelClosed,
+  setShellPanelCollapsed,
+  panelHeading,
+  panelClosed,
+}: InfoSidebarProps) {
+  const [modalOpen, setModalOpen] = useState(false)
+  const handlePanelClose = () => {
+    setPanelClosed(true)
+    setShellPanelCollapsed(true)
+  }
+
+  return (
+    <CalcitePanel
+      heading={panelHeading}
+      closed={panelClosed}
+      onCalcitePanelClose={handlePanelClose}
+    >
+      <CalciteBlock
+        className='text-start my-1'
+        // collapsible
+        heading='Map Details'
+        description='Check out what’s new in Version 3.1! Basemap blending is turned on
+          by default, the footprint layer can now be filtered by scale, and
+          there is a Geologic Unit Search tool found under the Map Config
+          Controls button.'
+      />
+
+      <CalciteBlock
+        className='text-start my-1'
+        heading='How To Use This Map'
+        description='The 3D map responds to keyboard navigation keys (←↑↓→), as well as mouse wheel zooming and drag panning.'
+      />
+
+      <CalciteBlock
+        className='text-start my-1'
+        heading='Data Sources'
+        description='The data used in this map is from the following sources: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      />
+      <div className='text-start mt-3 my-1 mx-2'>
+        <CalciteButton
+          alignment='center'
+          onClick={() => setModalOpen(!modalOpen)}
+        >
+          Open Data Disclaimer
+        </CalciteButton>
+      </div>
+
+      <CalciteModal
+        open={modalOpen}
+        onCalciteModalClose={() => setModalOpen(false)}
+      >
+        <h3 slot='header'>Data Disclaimer</h3>
+        <p slot='content'>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </CalciteModal>
+
+      <div className='text-start mt-3 my-1 mx-2 '>
+        <CalciteLink href='https://google.com/' target='_blank'>
+          Google
+        </CalciteLink>
+      </div>
+    </CalcitePanel>
+  )
+}
+
+export default InfoSidebar
