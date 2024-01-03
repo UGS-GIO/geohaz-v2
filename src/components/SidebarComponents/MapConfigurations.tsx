e61b6e9445a509c244e325c5e230c63ca35219b9
Clinton Lunn <cpl09c@gmail.com>
2024-01-03 19:54:12 +0000

    feat: map config changes

--- src/components/SidebarComponents/MapConfigurations.tsx
+++ src/components/SidebarComponents/MapConfigurations.tsx
@@ -1,49 +1,34 @@
 import {
   CalciteBlock,
   CalciteButton,
+  CalciteIcon,
+  CalciteLabel,
   CalciteLink,
+  CalciteNotice,
+  CalciteSegmentedControl,
+  CalciteSegmentedControlItem,
+  CalciteSlider,
 } from '@esri/calcite-components-react'
 
-function MapConfigurations () {
+
+function MapConfigurations() {
   return (
     <div>
-      {/* <CalcitePanel heading='Map Configurations'> */}
-      <CalciteBlock
-        className='text-start my-1'
-        // collapsible
-        heading='Map Details'
-        description='Check out what’s new in Version 3.1! Basemap blending is turned on
-          by default, the footprint layer can now be filtered by scale, and
-          there is a Geologic Unit Search tool found under the Map Config
-          Controls button.'
-      />
 
-      <CalciteBlock
-        className='text-start my-1'
-        heading='How To Use This Map'
-        description='The 3D map responds to keyboard navigation keys (←↑↓→), as well as mouse wheel zooming and drag panning.'
-      />
-
-      <CalciteBlock
-        className='text-start my-1'
-        heading='Data Sources'
-        description='The data used in this map is from the following sources: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-      />
-      <div className='text-start mt-3 my-1 mx-2'>
-        <CalciteButton
-          alignment='center'
-          onClick={() => console.log('nkl,agnakl')}
-        >
-          Open Data Disclaimer
-        </CalciteButton>
-      </div>
-
-      <div className='text-start mt-3 my-1 mx-2 '>
-        <CalciteLink href='https://google.com/' target='_blank'>
-          Google
-        </CalciteLink>
-      </div>
-      {/* </CalcitePanel> */}
+      <CalciteBlock open heading="Layer effects" description="Adjust blur, highlight, and more">
+        <CalciteIcon scale="s" slot="icon" icon="effects" />
+        <CalciteLabel>
+          Effect type
+          <CalciteSegmentedControl width="full">
+            <CalciteSegmentedControlItem value="Blur" />
+            <CalciteSegmentedControlItem checked value="Highlight" />
+          </CalciteSegmentedControl>
+        </CalciteLabel>
+        <CalciteLabel>
+          Effect intensity
+          <CalciteSlider />
+        </CalciteLabel>
+      </CalciteBlock>
     </div>
   )
 }
