295802dc69b441a450ccbedcd4c5b35901a6ad8f
Clinton Lunn <clunn@utah.gov>
2024-01-16 19:44:22 -0700

    ALL-1589: combine mapprovider with other providers in index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import './index.css';
 import '@esri/calcite-components/dist/calcite/calcite.css';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import { App } from './App';
+import { defineCustomElements } from '@esri/calcite-components/dist/loader';
+import { ThemeProvider } from './contexts/ThemeProvider';
+import { MapProvider } from './contexts/MapProvider';
+
 
 // Create a root element for the application
 const root = createRoot(document.querySelector('#root') as HTMLElement);
@@ -13,11 +15,6 @@ const root = createRoot(document.querySelector('#root') as HTMLElement);
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
-
-import { defineCustomElements } from '@esri/calcite-components/dist/loader';
-
-import { ThemeProvider } from './contexts/ThemeProvider';
-
 // CDN hosted assets
 defineCustomElements(window);
 
@@ -26,7 +23,9 @@ root.render(
   <StrictMode>
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <MapProvider>
+          <App />
+        </MapProvider>
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
