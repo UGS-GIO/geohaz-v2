3071f98b374564874b12bd524e1274035ec624c6
Clinton Lunn <clunn@utah.gov>
2024-01-18 18:44:23 -0700

    refactor: organize types and utils into their own files, make util functions more generic (ALL-1589)

--- src/config/mapping.ts
+++ src/config/mapping.ts
@@ -1,62 +1,10 @@
 import Map from '@arcgis/core/Map'
 import SceneView from '@arcgis/core/views/SceneView'
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
-import MapImageLayer from '@arcgis/core/layers/MapImageLayer'
-import TileLayer from '@arcgis/core/layers/TileLayer'
-import GroupLayer from '@arcgis/core/layers/GroupLayer'
 import layers from '../config/layers'
 import Color from '@arcgis/core/Color'
+import { LayerConfig, MapApp } from './types/mappingTypes'
+import { createLayer, setPopupAlignment } from './util/mappingUtils'
 
-interface LayerConfig {
-    type: 'feature' | 'tile' | 'map-image' | 'group'
-    url?: string
-    options?: object
-    title?: string
-    visible?: boolean
-    layers?: LayerConfig[]
-}
-
-interface MapApp {
-    view?: SceneView
-}
-
-const createLayer = (layerConfig: LayerConfig): FeatureLayer | TileLayer | GroupLayer | MapImageLayer | undefined => {
-    // Create a layer based on the layerConfig
-    switch (layerConfig.type) {
-        case 'feature':
-            if ('url' in layerConfig) {
-                return new FeatureLayer({
-                    url: layerConfig.url,
-                    ...layerConfig.options,
-                })
-            }
-            break
-        case 'tile':
-            if ('url' in layerConfig) {
-                return new TileLayer({
-                    url: layerConfig.url,
-                    ...layerConfig.options,
-                })
-            }
-            break
-        case 'map-image':
-            if ('url' in layerConfig) {
-                return new MapImageLayer({
-                    url: layerConfig.url,
-                    ...layerConfig.options,
-                })
-            }
-            break
-        case 'group':
-            return new GroupLayer({
-                title: layerConfig.title,
-                visible: layerConfig.visible,
-                layers: (layerConfig.layers?.map(createLayer).filter(layer => layer !== undefined) as (FeatureLayer | TileLayer | GroupLayer | MapImageLayer)[]), // Remove undefined values
-            })
-        default:
-            return undefined
-    }
-}
 
 // Create a global app object to store the view
 const app: MapApp = {}
@@ -87,12 +35,15 @@ export function init(container: HTMLDivElement) {
     })
 
     // Add layers to the map
-    layers.forEach((layerConfig: any) => {
+    layers.forEach((layerConfig: LayerConfig) => {
         const layer = createLayer(layerConfig)
         if (layer) {
             map.add(layer)
         }
     })
+
+    // prevent collision with the edges of the view
+    setPopupAlignment(view);
 
     return view
 }
