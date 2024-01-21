45efd3cf63bac52141e12798484c883066e81d04
Clinton Lunn <clunn@utah.gov>
2024-01-21 10:32:38 -0700

    feat: adding temp layerlist widget

--- src/components/widgets/MapWidgets.tsx
+++ src/components/widgets/MapWidgets.tsx
@@ -2,6 +2,7 @@ import Home from '@arcgis/core/widgets/Home';
 import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
 import Locate from '@arcgis/core/widgets/Locate';
 import useArcGISWidget from '../../hooks/useArcGISWidget';
+import LayerList from '@arcgis/core/widgets/LayerList';
 
 // Arcgos JS API Widgets that are overlaid on the map
 const MapWidgets: React.FC = () => {
@@ -9,6 +10,7 @@ const MapWidgets: React.FC = () => {
         { WrappedWidget: Home, position: 'top-left' },
         { WrappedWidget: BasemapToggle, position: 'bottom-right', config: { secondBasemap: 'satellite' } },
         { WrappedWidget: Locate, position: 'top-left' },
+        { WrappedWidget: LayerList, position: 'top-left' }
     ]);
 
     return null;
