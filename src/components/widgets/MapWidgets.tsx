1b9adc2e02d6b47ce637959c6e83587bbcb51759
Clinton Lunn <clunn@utah.gov>
2024-05-02 14:23:56 -0600

    feat: reorder map widgets (ALL-2135)

--- src/components/widgets/MapWidgets.tsx
+++ src/components/widgets/MapWidgets.tsx
@@ -2,7 +2,6 @@ import Home from '@arcgis/core/widgets/Home';
 import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
 import Locate from '@arcgis/core/widgets/Locate';
 import useArcGISWidget from '../../hooks/useArcGISWidget';
-import LayerList from '@arcgis/core/widgets/LayerList';
 import Legend from '@arcgis/core/widgets/Legend';
 
 // Arcgos JS API Widgets that are overlaid on the map
@@ -11,7 +10,6 @@ const MapWidgets: React.FC = () => {
         { WrappedWidget: Home, position: 'top-left' },
         { WrappedWidget: BasemapToggle, position: 'bottom-right', config: { secondBasemap: 'satellite' } },
         { WrappedWidget: Locate, position: 'top-left' },
-        { WrappedWidget: LayerList, position: 'top-left' },
         { WrappedWidget: Legend, position: 'top-left' }
     ]);
 
