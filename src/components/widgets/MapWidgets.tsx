663e6c8a6e6d9c34b57ad02eb29d660cf0e415dc
Clinton Lunn <clunn@utah.gov>
2024-05-02 12:07:59 -0600

    testing if removing feature widget fixes the error logs

--- src/components/widgets/MapWidgets.tsx
+++ src/components/widgets/MapWidgets.tsx
@@ -1,7 +1,7 @@
 import Home from '@arcgis/core/widgets/Home';
 import Locate from '@arcgis/core/widgets/Locate';
 import useArcGISWidget from '../../hooks/useArcGISWidget';
-import Feature from "@arcgis/core/widgets/Feature.js";
+// import Feature from "@arcgis/core/widgets/Feature.js";
 import Expand from '@arcgis/core/widgets/Expand';
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery.js";
 import { useContext } from 'react';
@@ -12,16 +12,16 @@ const MapWidgets: React.FC = () => {
     const { view, isMobile } = useContext(MapContext);
 
 
-    const coordinateFeatureConfig = {
-        id: 'coordinate-feature-widget',
-        graphic: {
-            popupTemplate: {
-                content: isMobile ? '' : 'Mouse over the map to update the coordinates.'
-            }
-        },
-        map: view?.map,
-        spatialReference: view?.spatialReference
-    };
+    // const coordinateFeatureConfig = {
+    //     id: 'coordinate-feature-widget',
+    //     graphic: {
+    //         popupTemplate: {
+    //             content: isMobile ? '' : 'Mouse over the map to update the coordinates.'
+    //         }
+    //     },
+    //     map: view?.map,
+    //     spatialReference: view?.spatialReference
+    // };
 
     const expandConfig = {
         id: 'basemap-gallery-expand',
@@ -36,11 +36,11 @@ const MapWidgets: React.FC = () => {
     useArcGISWidget([
         { WrappedWidget: Home, position: 'top-left' },
         { WrappedWidget: Locate, position: 'top-left' },
-        {
-            WrappedWidget: Feature,
-            position: isMobile ? 'top-right' : 'bottom-right',
-            config: coordinateFeatureConfig
-        },
+        // {
+        //     WrappedWidget: Feature,
+        //     position: isMobile ? 'top-right' : 'bottom-right',
+        //     config: coordinateFeatureConfig
+        // },
         {
             WrappedWidget: Expand,
             position: isMobile ? 'top-left' : 'top-right',
