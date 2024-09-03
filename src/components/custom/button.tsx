e367ccfd23bed8f908a7d25ea4f896de02f7528f
Clinton Lunn <cpl09c@gmail.com>
2024-09-03 13:26:36 -0600

    feat: add a basemap switcher to the top nav (#56)
    * feat: add a basemap switcher to the top nav
    
    * refactor: remove arcgis basemap widget

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils'
 import { IconLoader2 } from '@tabler/icons-react'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,47 +1,103 @@
-import { cn } from '@/lib/utils'
-import { Link } from 'react-router-dom'
+import { useContext, useState } from 'react';
+import { cn } from '@/lib/utils';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { Button } from './custom/button'
-import { IconMenu } from '@tabler/icons-react'
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/custom/button';
+import { IconMenu } from '@tabler/icons-react';
+import { MapContext } from '@/context/map-provider';
 
-interface TopNavProps extends React.HTMLAttributes<HTMLElement> {
-  links: {
-    title: string
-    href: string
-    isActive: boolean
-  }[]
+type BasemapType = {
+  title: string;
+  basemapStyle: string;
+  isActive: boolean;
+  type: 'short' | 'long';
+};
+
+const basemapList: BasemapType[] = [
+  { title: 'Streets', basemapStyle: 'streets', isActive: true, type: 'short' },
+  { title: 'Topographic', basemapStyle: 'topo', isActive: false, type: 'short' },
+  { title: 'Satellite', basemapStyle: 'satellite', isActive: false, type: 'short' },
+  { title: 'Hybrid', basemapStyle: 'hybrid', isActive: false, type: 'short' },
+  { title: 'Streets Relief', basemapStyle: 'streets-relief-vector', isActive: false, type: 'long' },
+  { title: 'Terrain', basemapStyle: 'terrain', isActive: false, type: 'long' },
+];
+
+interface TopNavProps extends React.HTMLAttributes<HTMLElement> { }
+
+interface BasemapProps {
+  links: BasemapType[];
+  trigger: React.ReactNode | string;
+  onBasemapChange: (basemapStyle: string) => void;
 }
 
-export function TopNav({ className, links, ...props }: TopNavProps) {
+const BasemapDropdown = ({ links, trigger, onBasemapChange }: BasemapProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+      <DropdownMenuContent side="bottom" align="start">
+        {links.map(({ title, basemapStyle, isActive }) => (
+          <DropdownMenuItem key={`${title}-${basemapStyle}`} asChild>
+            <Button
+              variant="ghost"
+              className={cn('w-full justify-start', !isActive ? 'text-muted-foreground' : 'underline')}
+              onClick={() => onBasemapChange(basemapStyle)}
+            >
+              {title}
+            </Button>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+function TopNav({ className, ...props }: TopNavProps) {
+  const { view } = useContext(MapContext);
+  const [basemaps, setBasemaps] = useState(basemapList);
+
+  const handleBasemapChange = (basemapStyle: string) => {
+    if (!view) return;
+    view.map.basemap = basemapStyle as unknown as __esri.Basemap;
+
+    // Update the active status across all basemaps
+    setBasemaps((prevBasemaps) =>
+      prevBasemaps.map((basemap) => ({
+        ...basemap,
+        isActive: basemap.basemapStyle === basemapStyle,
+      }))
+    );
+  };
+
+  // Check if any long-type basemap is active
+  const isLongActive = basemaps.some(({ type, isActive }) => type === 'long' && isActive);
+
+  const mobileTrigger = (
+    <Button size="icon" variant="outline">
+      <IconMenu />
+    </Button>
+  );
+  const desktopTrigger = (
+    <Button className={`text-stone-400 ${isLongActive ? 'text-primary-foreground underline' : ''} focus-visible:outline-none`} variant="ghost">
+      More
+    </Button>
+  );
+
   return (
     <>
-      <div className='md:hidden'>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button size='icon' variant='outline'>
-              <IconMenu />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent side='bottom' align='start'>
-            {links.map(({ title, href, isActive }) => (
-              <DropdownMenuItem key={`${title}-${href}`} asChild>
-                <Link
-                  to={href}
-                  className={!isActive ? 'text-muted-foreground' : ''}
-                >
-                  {title}
-                </Link>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+      {/* Mobile */}
+      <div className="md:hidden">
+        <BasemapDropdown
+          links={basemaps.filter(({ type }) => type === 'short')}
+          trigger={mobileTrigger}
+          onBasemapChange={handleBasemapChange}
+        />
       </div>
 
+      {/* Desktop */}
       <nav
         className={cn(
           'hidden items-center space-x-4 md:flex lg:space-x-6',
@@ -49,16 +105,26 @@ export function TopNav({ className, links, ...props }: TopNavProps) {
         )}
         {...props}
       >
-        {links.map(({ title, href, isActive }) => (
-          <Link
-            key={`${title}-${href}`}
-            to={href}
-            className={`text-sm font-medium transition-colors hover:text-primary ${isActive ? '' : 'text-muted-foreground'}`}
-          >
-            {title}
-          </Link>
-        ))}
+        {basemaps
+          .filter(({ type }) => type === 'short')
+          .map(({ title, basemapStyle, isActive }) => (
+            <Button
+              variant="ghost"
+              key={`${title}-${basemapStyle}`}
+              onClick={() => handleBasemapChange(basemapStyle)}
+              className={`text-sm font-medium transition-colors hover:text-primary-foreground ${isActive ? 'underline' : 'text-muted-foreground'}`}
+            >
+              {title}
+            </Button>
+          ))}
+        <BasemapDropdown
+          links={basemaps.filter(({ type }) => type === 'long')}
+          trigger={desktopTrigger}
+          onBasemapChange={handleBasemapChange}
+        />
       </nav>
     </>
-  )
+  );
 }
+
+export { TopNav };
