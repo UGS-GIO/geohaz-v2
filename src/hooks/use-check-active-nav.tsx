a699ddf1b5813229eed3f52c10062a6b2b9abf46
Clinton Lunn <cpl09c@gmail.com>
2024-09-03 13:18:29 -0600

    feat: zoom to layer and close menu when on mobile, refactor the useSidebar to lift state up into a sidbar context (#54)
    * feat: zoom to layer and close menu when on mobile, refactor the useSidebar to lift state up into a sidbar context
    
    * feat: enable the zoom to button to also trigger the group layer to be clicked

--- src/hooks/use-check-active-nav.tsx
+++ src/hooks/use-check-active-nav.tsx
@@ -3,7 +3,6 @@ import { useSidebar } from '@/hooks/use-sidebar'
 export default function useCheckActiveNav() {
   const { currentContent } = useSidebar()
 
-
   const checkActiveNav = (nav: string) => {
     if (!currentContent) return false
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,25 @@
+import '@/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/context/theme-provider'
 import router from '@/router'
-import '@/index.css'
-import { MapProvider } from './context/map-provider'
+import { MapProvider } from '@/context/map-provider'
 import { SidebarProvider } from '@/context/sidebar-provider'
+import { LayerListProvider } from '@/context/layerlist-provider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-        <MapProvider>
-          <RouterProvider router={router} />
-          <Toaster />
-        </MapProvider>
-      </ThemeProvider>
-    </SidebarProvider>
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <SidebarProvider>
+        <LayerListProvider>
+          <MapProvider>
+            <RouterProvider router={router} />
+            <Toaster />
+          </MapProvider>
+        </LayerListProvider>
+      </SidebarProvider>
+    </ThemeProvider>
   </React.StrictMode >
 )
