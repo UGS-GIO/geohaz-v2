9b81d0e14bc047e07c0141e60b43f8c75eda7df3
Clinton Lunn <clunn@utah.gov>
2024-09-30 16:09:15 -0600

    refactor: add Link variants, create social icon svgs, add social links

--- src/pages/dashboard/components/map-coordinates.tsx
+++ src/pages/dashboard/components/map-coordinates.tsx
@@ -24,6 +24,7 @@
 // );
 
 
+import { Switch } from "@/components/ui/switch";
 import { MapContext } from "@/context/map-provider";
 import { useMapCoordinates } from "@/hooks/use-map-coordinates";
 import { addCommas } from "@/lib/utils";
@@ -35,8 +36,17 @@ const MapCoordinates = () => {
         coordinates,
         scale,
         handleDesktopViewChange,
+        isDecimalDegrees,
+        setIsDecimalDegrees,
     } = useMapCoordinates();
 
+    const handleOnCheckedChange = () => (checked: boolean) => {
+        if (checked) {
+            setIsDecimalDegrees(false)
+        } else {
+            setIsDecimalDegrees(true)
+        }
+    }
 
     // Use desktop or mobile view handlers based on device type
     useEffect(() => {
@@ -56,13 +66,35 @@ const MapCoordinates = () => {
         };
     }, [view, isMobile, handleDesktopViewChange]);
 
+    const UnitSwitch = () => (
+        <>
+            {/* decimal degrees */}
+            <span className="text-sm text-muted-foreground">DD</span>
+            <Switch
+                checked={isDecimalDegrees ? false : true}
+                onCheckedChange={handleOnCheckedChange()}
+                aria-label="Toggle coordinate format"
+                // Use the primary color for the unchecked state also
+                // This is not an on/off toggle. it toggles between DD (left) and DMS (right)
+                // ossible non-standard use of the switch component
+                className="data-[state=unchecked]:bg-primary"
+            />
+            {/* degrees minutes seconds */}
+            <span className="text-sm text-muted-foreground">DMS</span>
+        </>
+    )
+
     return (
         <div className="flex items-center space-x-2">
-            <span className="text-sm text-muted-foreground">Lat:{' '}{coordinates.y}</span>
-            <span className="text-sm text-muted-foreground">Lon:{' '}{coordinates.x}</span>
-            <span className="text-sm text-muted-foreground">
-                Scale: 1:{addCommas(scale?.toFixed(0).toString() || '')}
-            </span>
+            <div className="flex items-center space-x-2">
+                <span className="text-sm text-muted-foreground">Lat:{' '}{coordinates.y}</span>
+                <span className="text-sm text-muted-foreground">Lon:{' '}{coordinates.x}</span>
+                <UnitSwitch />
+                <div className="h-4 w-px bg-border" aria-hidden="true" />
+                <span className="text-sm text-muted-foreground">
+                    Scale: 1:{addCommas(scale?.toFixed(0).toString() || '')}
+                </span>
+            </div>
         </div>
     );
 };
--- src/pages/dashboard/components/map-footer.tsx
+++ src/pages/dashboard/components/map-footer.tsx
@@ -1,64 +1,46 @@
 import { Link } from "@/components/custom/link"
-import { Facebook, Twitter, Instagram, Github } from "lucide-react"
+import { FacebookIcon, InstagramIcon, XIcon, LinkedinIcon, GithubIcon } from "@/assets/social-icons"
 import { MapCoordinates } from "@/pages/dashboard/components/map-coordinates"
-import { Switch } from "@/components/ui/switch"
-import { useMapCoordinates } from "@/hooks/use-map-coordinates"
+import { ShoppingCart } from "lucide-react"
 
 const MapFooter = () => {
-    const { isDecimalDegrees, setIsDecimalDegrees } = useMapCoordinates()
-
-    const handleOnCheckedChange = () => (checked: boolean) => {
-        if (checked) {
-            setIsDecimalDegrees(false)
-        } else {
-            setIsDecimalDegrees(true)
-        }
-    }
-
     return (
         <>
             <div className="flex items-center space-x-4">
-                <span className="text-sm font-medium">Utah Geological Survey</span>
+                <Link to="https://geology.utah.gov/" variant='foreground'>
+                    <span className="text-sm font-medium">Utah Geological Survey</span>
+                </Link>
                 <div className="h-4 w-px bg-border" aria-hidden="true" />
                 <div className="flex items-center space-x-4">
                     <div className="flex space-x-2">
-                        <Link to="#">
-                            <Facebook className='stroke-foreground h-5 w-5' />
+                        <Link to="https://www.facebook.com/pages/Utah-Geological-Survey/251490738585">
+                            <FacebookIcon className='stroke-foreground h-5 w-5' />
                             <span className="sr-only">Facebook</span>
                         </Link>
-                        <Link to="#">
-                            <Twitter className='stroke-foreground h-5 w-5' />
-                            <span className="sr-only">Twitter</span>
+                        <Link to="https://x.com/utahgeological">
+                            <XIcon className='stroke-foreground h-5 w-5' />
+                            <span className="sr-only">X</span>
                         </Link>
-                        <Link to="#">
-                            <Instagram className='stroke-foreground h-5 w-5' />
+                        <Link to="https://www.instagram.com/utahgeologicalsurvey/">
+                            <InstagramIcon className='stroke-foreground h-5 w-5' />
                             <span className="sr-only">Instagram</span>
                         </Link>
-                        <Link to="#">
-                            <Github className='stroke-foreground h-5 w-5' />
+                        <Link to="http://www.linkedin.com/company/utah-geological-survey">
+                            <span className="sr-only">LinkedIn</span>
+                            <LinkedinIcon className='stroke-foreground h-5 w-5' />
+                        </Link>
+                        <Link to="https://github.com/UGS-GIO/geohaz-v2">
+                            <GithubIcon className='stroke-foreground h-5 w-5' />
                             <span className="sr-only">GitHub</span>
+                        </Link>
+                        <Link to="https://utahmapstore.com/">
+                            <span className="sr-only">Utah Geological Survey</span>
+                            <ShoppingCart className='stroke-foreground h-5 w-5' />
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className="flex items-center space-x-2">
-                {/* decimal degrees */}
-                <span className="text-sm text-muted-foreground">DD</span>
-                <Switch
-                    checked={isDecimalDegrees ? false : true}
-                    onCheckedChange={handleOnCheckedChange()}
-                    aria-label="Toggle coordinate format"
-                    // Use the primary color for the unchecked state also
-                    // This is not an on/off toggle. it toggles between DD (left) and DMS (right)
-                    // ossible non-standard use of the switch component
-                    className="data-[state=unchecked]:bg-primary"
-                />
-                {/* degrees minutes seconds */}
-                <span className="text-sm text-muted-foreground">DMS</span>
-                <span className="text-sm">
-                    <MapCoordinates />
-                </span>
-            </div>
+            <MapCoordinates />
         </>
     )
 }
