53e065a275f9f5b22d1a68ed6713f5ec0cd4e879
Clinton Lunn <clunn@utah.gov>
2024-10-09 09:43:11 -0400

    refactor: fix social-icons layer props

--- src/assets/social-icons.tsx
+++ src/assets/social-icons.tsx
@@ -13,9 +13,9 @@ const FacebookIcon: React.FC<FacebookIconProps> = ({ className }) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn('lucide lucide-facebook', className)}
     >
         <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
@@ -35,9 +35,9 @@ const XIcon: React.FC<XIconProps> = ({ className }) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentcolor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn("lucide", className)}>
         <path d="M8,2H3L16.7,22h5.1L8,2z" /> <line x1="2.3" y1="22.1" x2="10.2" y2="12.8" /> <line x1="19.8" y1="2" x2="13.3" y2="9.6" />
     </svg>
@@ -56,9 +56,9 @@ const InstagramIcon: React.FC<InstagramIconProps> = ({ className }) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn("lucide lucide-instagram", className)}
     >
         <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -78,9 +78,9 @@ const LinkedinIcon: React.FC<LinkedinIconProps> = ({ className }) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn("lucide", className)}
     >
         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
@@ -101,9 +101,9 @@ const GithubIcon: React.FC<GithubIconProps> = ({ className }) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn("lucide lucide-github", className)}>
         <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
         <path d="M9 18c-4.51 2-5-2-7-2" />
