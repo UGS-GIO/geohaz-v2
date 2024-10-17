60250d1fdf7ec511c60a2d533ac2ba5c9cbe14ca
Clinton Lunn <cpl09c@gmail.com>
2024-10-17 10:11:54 -0600

    refactor: remove layerlst viewmodel from setting activelayers, fix symbol generation (#66)
    * refactor: remove layerlist viewmodel for managing activelayers
    
    * refactor: fixing symbol generation for layerlist legend
    
    * refactor: limiting requests that are sent to geoserver to retrieve legend info using @tanstack/react-query for caching and only searching for single renderer at a time

--- src/context/map-provider.tsx
+++ src/context/map-provider.tsx
@@ -1,19 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import type SceneView from "@arcgis/core/views/SceneView";
 import type MapView from "@arcgis/core/views/MapView";
-import LayerList from "@arcgis/core/widgets/LayerList";
 import * as reactiveUtils from "@arcgis/core/core/reactiveUtils.js";
-// import { useQuery } from "@tanstack/react-query";
-import { RendererProps } from "@/lib/types/mapping-types";
 import { useFetchLayerDescriptions } from "@/hooks/use-fetch-layer-descriptions";
-
 
 type MapContextProps = {
     view?: SceneView | MapView,
-    activeLayers?: __esri.Collection<__esri.ListItem>, // add a layers property to the context
+    activeLayers?: __esri.Collection<__esri.Layer>, // add a layers property to the context
     loadMap?: (container: HTMLDivElement) => Promise<void>
-    setActiveLayers?: (layers: __esri.Collection<__esri.ListItem>) => void
-    getRenderer?: (id: string, url: string) => Promise<RendererProps | undefined>
+    setActiveLayers?: (layers: __esri.Collection<__esri.Layer>) => void
     isMobile?: boolean
     setIsMobile?: (isMobile: boolean) => void
     layerDescriptions?: Record<string, string>
@@ -43,7 +38,6 @@ export const MapContext = createContext<MapContextProps>({
     activeLayers: undefined,
     loadMap: async () => { },
     setActiveLayers: () => { },
-    getRenderer: async () => { return undefined },
     isMobile: false,
     setIsMobile: () => { },
     layerDescriptions: {},
@@ -58,7 +52,7 @@ const fetchLayerDescriptions = async (setLayerDescriptions: (descriptions: Recor
 
 export function MapProvider({ children }: { children: React.ReactNode }) {
     const [view, setView] = useState<SceneView | MapView>();
-    const [activeLayers, setActiveLayers] = useState<__esri.Collection<__esri.ListItem>>();
+    const [activeLayers, setActiveLayers] = useState<__esri.Collection<__esri.Layer>>();
     const [coordinates, setCoordinates] = useState<{ x: string; y: string }>({ x: "000.000", y: "000.000" });
     const [isMobile, setIsMobile] = useState<boolean>(false);
     const [isDecimalDegrees, setIsDecimalDegrees] = useState<boolean>(true);
@@ -85,20 +79,8 @@ export function MapProvider({ children }: { children: React.ReactNode }) {
 
     useEffect(() => {
         if (!view) return;
-
-        // Create LayerList widget
-        const layerList = new LayerList({
-            view: view,
-        });
-
-        // Get LayerList view model
-        const layerListViewModel = layerList.viewModel;
-
-        // Access operational items
-        const operationalItems = layerListViewModel.operationalItems;
-
         // Update the active layers in the context
-        setActiveLayers(operationalItems);
+        setActiveLayers(view.map.layers);
 
         // Determine if the view is mobile
         reactiveUtils.watch(() => [view.heightBreakpoint, view.widthBreakpoint],
@@ -114,17 +96,8 @@ export function MapProvider({ children }: { children: React.ReactNode }) {
 
         setIsMobile(view.widthBreakpoint === "xsmall" || view.heightBreakpoint === "xsmall");
         fetchLayerDescriptions(setLayerDescriptions);
-
-        // Clean up - destroy the LayerList widget when component unmounts
-        return () => {
-            layerList.destroy();
-        };
-
     }, [view]);
 
-    useEffect(() => {
-        console.log(layerDescriptions);
-    }, [layerDescriptions]);
     async function loadMap(container: HTMLDivElement) {
         if (view) return;
         const { init } = await import("@/lib/mapping-utils")
@@ -134,7 +107,7 @@ export function MapProvider({ children }: { children: React.ReactNode }) {
 
 
     return (
-        <MapContext.Provider value={{ view, loadMap, activeLayers, setActiveLayers, isMobile, setIsMobile /* , layerDescriptions */, isDecimalDegrees, setIsDecimalDegrees, coordinates, setCoordinates }}>
+        <MapContext.Provider value={{ view, loadMap, activeLayers, setActiveLayers, isMobile, setIsMobile, layerDescriptions, isDecimalDegrees, setIsDecimalDegrees, coordinates, setCoordinates }}>
             {children}
         </MapContext.Provider>
     )
