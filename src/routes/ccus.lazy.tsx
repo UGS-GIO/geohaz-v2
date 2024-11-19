b0c43caad2cefa26ca2000e8a382b70c0d759f7b
Clinton Lunn <clunn@utah.gov>
2024-11-19 09:48:50 -0700

    refactor: putting the sidebar into each page, and removing from __root.tsx

--- src/routes/ccus.lazy.tsx
+++ src/routes/ccus.lazy.tsx
@@ -1,5 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import Map from '@/pages/ccus/page'
+import Map from '@/pages/ccus'
 
 export const Route = createLazyFileRoute('/ccus')({
     component: () => <Map />,
