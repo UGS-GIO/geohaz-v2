797c275aeb7ea9af387baa9a9a82b0120bc26ead
Clinton Lunn <clunn@utah.gov>
2024-12-07 15:19:36 -0700

    refactor: track carousel with scrolling again, refactor ui of popup cards

--- src/components/custom/popups/popup-content-with-pagination.tsx
+++ src/components/custom/popups/popup-content-with-pagination.tsx
@@ -9,25 +9,25 @@ import proj4 from 'proj4';
 import { MapContext } from "@/context/map-provider"
 import { highlightFeature, fetchWfsGeometry, convertBbox } from '@/lib/mapping-utils';
 import Extent from "@arcgis/core/geometry/Extent"
-
-
 import { useGetPopupButtons } from "@/hooks/use-get-popup-buttons"
-import { Separator } from "@/components/ui/separator"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 const ITEMS_PER_PAGE_OPTIONS = [1, 5, 10, 25, 50, Infinity] // 'Infinity' for 'All'
 
 export interface ExtendedFeature extends Feature<Geometry, GeoJsonProperties> {
-    namespace: string; // Add the namespace property
+    namespace: string;
+}
+
+interface LayerContentProps {
+    groupLayerTitle: string
+    layerTitle: string
+    features: ExtendedFeature[]
+    popupFields?: Record<string, string>
+    relatedTables?: RelatedTable[]
 }
 
 interface SidebarInsetWithPaginationProps {
-    layerContent: {
-        groupLayerTitle: string
-        layerTitle: string
-        features: ExtendedFeature[]
-        popupFields?: Record<string, string>
-        relatedTables?: RelatedTable[]
-    }[]
+    layerContent: LayerContentProps[]
     onSectionChange: (layerTitle: string) => void
 }
 
@@ -37,55 +37,142 @@ interface PopupPaginationProps {
     handlePageChange: (page: number) => void
     itemsPerPage: number
     onItemsPerPageChange: (size: number) => void
-    showPagination: boolean
 }
 
-const PopupPagination = ({ currentPage, totalPages, handlePageChange, itemsPerPage, onItemsPerPageChange, showPagination }: PopupPaginationProps) => {
+const PopupPagination = ({ currentPage, totalPages, handlePageChange, itemsPerPage, onItemsPerPageChange }: PopupPaginationProps) => {
     return (
-        <>
-            {showPagination && (
-                <div className="flex items-center justify-between px-4 py-2 w-full bg-background">
-                    <div className="flex-1 text-sm text-muted-foreground">
-                        Page {currentPage} of {totalPages}
+        <div className="flex items-center justify-between w-full">
+            <div className="flex-1 text-sm text-muted-foreground">
+                Page {currentPage} of {totalPages}
+            </div>
+            <div className="flex items-center space-x-2">
+                <Select
+                    value={`${itemsPerPage}`}
+                    onValueChange={(value) => onItemsPerPageChange(value === "Infinity" ? Infinity : Number(value))}
+                >
+                    <SelectTrigger>
+                        <SelectValue placeholder={itemsPerPage.toString()} />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {ITEMS_PER_PAGE_OPTIONS.map((option) => (
+                            <SelectItem key={option} value={`${option}`}>
+                                {option === Infinity ? "All" : option}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
+                <Button variant="outline" onClick={() => handlePageChange(1)} disabled={currentPage === 1} className="h-8 w-8 p-0">
+                    <ChevronFirst className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="h-8 w-8 p-0">
+                    <ChevronLeft className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className="h-8 w-8 p-0">
+                    <ChevronRight className="h-4 w-4" />
+                </Button>
+                <Button variant="outline" onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} className="h-8 w-8 p-0">
+                    <ChevronLast className="h-4 w-4" />
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+const LayerCard = ({
+    layer,
+    buttons,
+    handleZoomToFeature
+}: {
+    layer: LayerContentProps,
+    buttons: React.ReactNode[] | null,
+    handleZoomToFeature: (feature: ExtendedFeature) => Promise<void>
+}) => {
+    const [itemsPerPage, setItemsPerPage] = useState(ITEMS_PER_PAGE_OPTIONS[0])
+    const [currentPage, setCurrentPage] = useState(1)
+
+    // Calculate total pages based on items per page
+    const totalPages = useMemo(() =>
+        Math.ceil(layer.features.length / itemsPerPage),
+        [layer.features, itemsPerPage]
+    )
+
+    // Paginate features for this layer
+    const paginatedFeatures = useMemo(() => {
+        const startIndex = (currentPage - 1) * itemsPerPage
+        return layer.features.slice(startIndex, startIndex + itemsPerPage)
+    }, [layer.features, currentPage, itemsPerPage])
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page)
+    }
+
+    const PopupButtons = ({ feature }: { feature: ExtendedFeature }) => (
+        <div className="flex justify-start gap-2">
+            <Button onClick={() => handleZoomToFeature(feature)} variant={'secondary'}>
+                Zoom to Feature
+            </Button>
+            {buttons && buttons.map((button) => button)}
+        </div>
+    )
+
+    return (
+        <Card
+            id={`section-${layer.layerTitle !== '' ? layer.layerTitle : layer.groupLayerTitle}`}
+            className="w-full"
+        >
+            <CardHeader>
+                <CardTitle>
+                    {layer.groupLayerTitle}
+                    {layer.layerTitle && ` - ${layer.layerTitle}`}
+                </CardTitle>
+                {layer.features.length > ITEMS_PER_PAGE_OPTIONS[0] && (
+                    <PopupPagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        handlePageChange={handlePageChange}
+                        itemsPerPage={itemsPerPage}
+                        onItemsPerPageChange={setItemsPerPage}
+                    />
+                )}
+            </CardHeader>
+            <CardContent className="space-y-4">
+                {paginatedFeatures.map((feature, idx) => (
+                    <div
+                        key={idx}
+                        className={`
+                            space-y-4 
+                            p-4 
+                            rounded-lg 
+                            ${idx % 2 === 0 ? 'bg-secondary/10' : 'bg-secondary/5'}
+                            border 
+                            border-secondary/20
+                        `}
+                    >
+                        {/* <div className="space-y-2">
+                            <h4 className="text-lg font-semibold text-foreground">
+                                Feature {idx + 1}
+                                {layer.layerTitle && ` - ${layer.layerTitle}`}
+                            </h4>
+                            <Separator decorative className="bg-secondary" />
+                        </div> */}
+
+                        <PopupButtons feature={feature} />
+
+                        <GenericPopup
+                            feature={feature}
+                            layout={layer.popupFields &&
+                                Object.keys(layer.popupFields).length > 5 ? "grid" : "stacked"}
+                            popupFields={layer.popupFields || {}}
+                            relatedTable={layer.relatedTables}
+                        />
                     </div>
-                    <div className="flex items-center space-x-2">
-                        <Select
-                            value={`${itemsPerPage}`}
-                            onValueChange={(value) => onItemsPerPageChange(value === "Infinity" ? Infinity : Number(value))}
-                        >
-                            <SelectTrigger>
-                                <SelectValue placeholder={itemsPerPage.toString()} />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {ITEMS_PER_PAGE_OPTIONS.map((option) => (
-                                    <SelectItem key={option} value={`${option}`}>
-                                        {option === Infinity ? "All" : option}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                        <Button variant="outline" onClick={() => handlePageChange(1)} disabled={currentPage === 1} className="h-8 w-8 p-0">
-                            <ChevronFirst className="h-4 w-4" />
-                        </Button>
-                        <Button variant="outline" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className="h-8 w-8 p-0">
-                            <ChevronLeft className="h-4 w-4" />
-                        </Button>
-                        <Button variant="outline" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className="h-8 w-8 p-0">
-                            <ChevronRight className="h-4 w-4" />
-                        </Button>
-                        <Button variant="outline" onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages} className="h-8 w-8 p-0">
-                            <ChevronLast className="h-4 w-4" />
-                        </Button>
-                    </div>
-                </div>
-            )}
-        </>
+                ))}
+            </CardContent>
+        </Card>
     )
 }
 
 const PopupContentWithPagination = ({ layerContent, onSectionChange }: SidebarInsetWithPaginationProps) => {
-    const [itemsPerPage, setItemsPerPage] = useState(ITEMS_PER_PAGE_OPTIONS[0])
-    const [paginationStates, setPaginationStates] = useState<{ [layerTitle: string]: number }>({})
     const { view } = useContext(MapContext)
     const buttons = useGetPopupButtons()
 
@@ -95,33 +182,26 @@ const PopupContentWithPagination = ({ layerContent, onSectionChange }: SidebarIn
     )
 
     useEffect(() => {
-        // Create an observer instance with specific options
         const observer = new IntersectionObserver(
             (entries) => {
-                // Get all currently intersecting sections
                 const visibleSections = entries.filter(entry => entry.isIntersecting)
 
                 if (visibleSections.length > 0) {
-                    // Find the topmost visible section
                     const topmostSection = visibleSections.reduce((prev, current) => {
-                        return prev.boundingClientRect.top > current.boundingClientRect.top ? current : prev
-                    })
+                        return (prev.boundingClientRect.top < current.boundingClientRect.top) ? prev : current;
+                    });
 
-                    // setActiveLayerTitle(topmostSection.target.id.replace('section-', ''))
-                    onSectionChange(topmostSection.target.id.replace('section-', ''))
+                    const sectionTitle = topmostSection.target.id.replace('section-', '');
+                    onSectionChange(sectionTitle);
                 }
             },
             {
-                // Root is the scrollable container
-                root: document.querySelector('.scrollable-container'),
-                // Small positive top margin to detect elements just before they stick
-                rootMargin: '0px 0px -90% 0px',
-                // Multiple thresholds to track visibility more precisely
-                threshold: 0.2  // Single threshold - when 20% of the element is visible
+                root: null, // Use the viewport as the root
+                rootMargin: '0px 0px -50% 0px', // 50% of the section must be visible
+                threshold: 0 // Trigger as soon as any part of the section enters the viewport
             }
         )
 
-        // Observe all section containers
         sectionIds.forEach((id) => {
             const element = document.getElementById(id)
             if (element) {
@@ -130,136 +210,53 @@ const PopupContentWithPagination = ({ layerContent, onSectionChange }: SidebarIn
         })
 
         return () => observer.disconnect()
-    }, [sectionIds])
+    }, [sectionIds, onSectionChange])
 
+    // Define coordinate systems
+    proj4.defs("EPSG:26912", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs");
+    proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
 
-    const handlePageChange = (layerTitle: string, page: number) => {
-        setPaginationStates((prevState) => ({
-            ...prevState,
-            [layerTitle]: page,
-        }))
-    }
+    const handleZoomToFeature = async (feature: ExtendedFeature) => {
+        const wfsGeometry = await fetchWfsGeometry({
+            namespace: feature.namespace,
+            featureId: feature.id!.toString()
+        });
 
-    const renderPaginatedFeatures = (
-        features: ExtendedFeature[],
-        popupFields: Record<string, string>,
-        relatedTables: RelatedTable[],
-        layerTitle: string,
-        groupLayerTitle: string
-    ) => {
-        const currentPage = paginationStates[layerTitle] || 1
-        const paginatedFeatures = features.slice(
-            (currentPage - 1) * itemsPerPage,
-            currentPage * itemsPerPage
-        )
+        highlightFeature(wfsGeometry.features[0], view!, {
+            fillColor: [0, 0, 0, 0],
+            outlineColor: [255, 255, 0, 1],
+            outlineWidth: 4,
+            pointSize: 12
+        });
 
-        // Determine layout based on number of fields
-        const layout = Object.keys(popupFields).length > 5 ? "grid" : "stacked"
+        if (feature.bbox) {
+            const bbox = convertBbox(feature.bbox)
 
-        // Define coordinate systems (do this once, outside the function)
-        proj4.defs("EPSG:26912", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs");
-        proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
-
-        const handleZoomToFeature = async (feature: ExtendedFeature) => {
-            const wfsGeometry = await fetchWfsGeometry({
-                namespace: feature.namespace,
-                featureId: feature.id!.toString()
+            view?.goTo({
+                target: new Extent({
+                    xmin: bbox[0],
+                    ymin: bbox[1],
+                    xmax: bbox[2],
+                    ymax: bbox[3],
+                    spatialReference: { wkid: 4326 }
+                })
             });
-
-            // Use the highlight utility
-            highlightFeature(wfsGeometry.features[0], view!, {
-                fillColor: [0, 0, 0, 0],
-                outlineColor: [255, 255, 0, 1],
-                outlineWidth: 4,
-                pointSize: 12
-            });
-
-            if (feature.bbox) {
-                const bbox = convertBbox(feature.bbox)
-
-                // Zoom to the feature
-                view?.goTo({
-                    target: new Extent({
-                        xmin: bbox[0],
-                        ymin: bbox[1],
-                        xmax: bbox[2],
-                        ymax: bbox[3],
-                        spatialReference: { wkid: 4326 } // WGS84
-                    })
-                });
-            }
         }
-
-        const PopupButtons = ({ feature }: { feature: ExtendedFeature }) => (
-            <div className="flex justify-start gap-2">
-                <Button onClick={() => handleZoomToFeature(feature)} variant={'secondary'}>
-                    Zoom to Feature
-                </Button>
-                {buttons && buttons.map((button) => button)} {/* Render popup buttons */}
-            </div>
-        )
-
-        return (
-            <div className="scroll-smooth">
-                <div className="space-y-4">
-                    {paginatedFeatures.map((feature, idx) => (
-                        <div className="border border-secondary p-4 rounded space-y-2 gap-4" key={idx}>
-                            {/* put the title and pagination here */}
-                            <div className="flex flex-col gap-2">
-                                <h3 className="text-xl font-semibold text-primary">
-                                    {groupLayerTitle}
-                                    {layerTitle && ` - ${layerTitle}`}
-                                </h3>
-                                <PopupPagination
-                                    showPagination={features.length > ITEMS_PER_PAGE_OPTIONS[0]}
-                                    currentPage={paginationStates[layerTitle] || 1}
-                                    totalPages={Math.ceil(features.length / itemsPerPage)}
-                                    handlePageChange={(page) => handlePageChange(layerTitle, page)}
-                                    itemsPerPage={itemsPerPage}
-                                    onItemsPerPageChange={setItemsPerPage}
-                                />
-                            </div>
-                            <Separator decorative className="bg-secondary" />
-                            <PopupButtons feature={feature} />
-                            {/* Popup content */}
-                            <GenericPopup
-                                feature={feature}
-                                layout={layout}
-                                popupFields={popupFields}
-                                relatedTable={relatedTables}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
     }
+
+    // If no layers, return null
+    if (layerContent.length === 0) return null;
 
     return (
         <div className="flex flex-1 flex-col gap-4 px-2 overflow-y-auto select-text h-full scrollable-container">
-            {layerContent.map((layer) => {
-                const features = layer.features
-                // const title = layer.layerTitle !== '' ? layer.layerTitle : layer.groupLayerTitle
-                // const sectionId = `section-${title}`
-
-                return (
-                    <div key={layer.layerTitle} className="relative">
-                        {features.length > 0 && (
-                            <div>
-                                <div className="mt-4">
-                                    {renderPaginatedFeatures(
-                                        features,
-                                        layer.popupFields || {},
-                                        layer.relatedTables || [],
-                                        layer.layerTitle,
-                                        layer.groupLayerTitle
-                                    )}
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                )
-            })}
+            {layerContent.map((layer, layerIndex) => (
+                <LayerCard
+                    key={layerIndex}
+                    layer={layer}
+                    buttons={buttons}
+                    handleZoomToFeature={handleZoomToFeature}
+                />
+            ))}
         </div>
     )
 }
