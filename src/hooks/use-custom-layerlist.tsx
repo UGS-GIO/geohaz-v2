732caf91ad9bc7b33dd5e256d78bf3ca11a39f8f
Clinton Lunn <clunn@utah.gov>
2025-02-11 14:18:59 -0700

    refactor: make the accordion open if the switch is checked by default

--- src/hooks/use-custom-layerlist.tsx
+++ src/hooks/use-custom-layerlist.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent, AccordionHeader } from '@/components/ui/accordion';
 import { Checkbox } from '@/components/ui/checkbox';
 import { MapContext } from '@/context/map-provider';
@@ -146,13 +146,13 @@ const LayerAccordion = ({ layer, isTopLevel, forceUpdate, onVisibilityChange }: 
 };
 
 const GroupLayerItem = ({ layer, index }: { layer: __esri.GroupLayer; index: number }) => {
-
     const { handleToggleAll, handleChildLayerToggle, handleGroupVisibilityToggle, localState, accordionTriggerRef } = useLayerVisibilityManager(layer);
     const invertedChildLayers = [...layer.layers].reverse(); // inverse the order of the layers to match the order in the map
+    const defaultValues = [`${localState.groupVisibility ? `item-${index}` : ''}`]; // if group is visible, expand the accordion
 
     return (
         <div className="mr-2 border border-secondary rounded my-2">
-            <Accordion type="multiple">
+            <Accordion type="multiple" defaultValue={defaultValues}>
                 <AccordionItem value={`item-${index}`}>
                     <AccordionHeader>
                         <Switch
@@ -189,6 +189,7 @@ const GroupLayerItem = ({ layer, index }: { layer: __esri.GroupLayer; index: num
         </div>
     );
 };
+
 
 const useCustomLayerList = () => {
     const { activeLayers } = useContext(MapContext);
