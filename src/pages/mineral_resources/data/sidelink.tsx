40f3cb00b1e8b1b83df22234b4b5bb8f84d1df51
Clinton Lunn <clunn@utah.gov>
2025-02-25 16:24:37 -0700

    feat: add mineral_resources app

---
+++ src/pages/mineral_resources/data/sidelink.tsx
@@ -0,0 +1,51 @@
+import { House, Info as InfoIcon, Layers as LayersIcon, Settings, ExternalLink } from 'lucide-react'
+import Info from '@/components/sidebar/info'
+import Layers from '@/components/sidebar/layers'
+import MapConfigurations from '@/components/sidebar/map-configurations'
+
+export interface NavLink {
+  title: string
+  label?: string
+  href?: string
+  icon: JSX.Element
+  component?: () => JSX.Element
+  componentPath?: string
+}
+
+export interface SideLink extends NavLink {
+  sub?: NavLink[]
+}
+
+export const sidelinks: SideLink[] = [
+  {
+    title: 'Home',
+    label: '',
+    icon: <House className='stroke-foreground' />,
+  },
+  {
+    title: 'Info',
+    label: '',
+    icon: <InfoIcon className='stroke-foreground' />,
+    component: Info, // Direct component reference
+    componentPath: 'src/components/sidebar/info.tsx',
+  },
+  {
+    title: 'Layers',
+    label: '',
+    icon: <LayersIcon className='stroke-foreground' />,
+    component: Layers, // Direct component reference
+  },
+  {
+    title: 'Map Configurations',
+    label: '',
+    icon: <Settings className='stroke-foreground' />,
+    component: MapConfigurations, // Direct component reference
+  },
+  {
+    title: 'Learn More',
+    label: '',
+    href: 'https://geology.utah.gov/energy-minerals/ccus/',
+    icon: <ExternalLink className='stroke-foreground' />,
+    // This link is external and does not need a component
+  }
+];
---
+++ src/routes/mineral_resources/index.lazy.tsx
@@ -0,0 +1,6 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+import Map from '@/pages/mineral_resources'
+
+export const Route = createLazyFileRoute('/mineral_resources/')({
+    component: () => <Map />,
+})
