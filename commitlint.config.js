d13016be407e26809d30b84072ed1b1c3bc41ea1
Clinton Lunn <cpl09c@gmail.com>
2025-05-01 23:02:07 -0600

    ci(common): relax the commit length rules (#217) (#218)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,8 @@ export default {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'subject-case': [0], // Allow any case in subject
-        'scope-enum': [2, 'always', ['common', 'hazards', 'minerals', 'ccus', 'wetlands', 'release']] // Define allowed scopes
+        'scope-enum': [2, 'always', ['common', 'hazards', 'minerals', 'ccus', 'wetlands', 'release']],
+        'footer-max-line-length': [1, 'always', 200],
+        'body-max-line-length': [1, 'always', 200]
     }
 };
