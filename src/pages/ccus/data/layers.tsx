9cb6b3a39900b6d056dabb3abe9fbde77e63efe7
Clinton Lunn <clunn@utah.gov>
2025-07-16 14:19:35 -0600

    feat(ccus): add major roads layer

--- src/pages/ccus/data/layers.tsx
+++ src/pages/ccus/data/layers.tsx
@@ -173,6 +173,26 @@ const riversWMSConfig: WMSLayerProps = {
     ],
 };
 
+// Roads WMS Layer
+const roadsLayerName = 'ccus_majorroads';
+const roadsWMSTitle = 'Major Roads';
+const roadsWMSConfig: WMSLayerProps = {
+    type: 'wms',
+    url: `${PROD_GEOSERVER_URL}/wms`,
+    title: roadsWMSTitle,
+    visible: false,
+    sublayers: [
+        {
+            name: `${ENERGY_MINERALS_WORKSPACE}:${roadsLayerName}`,
+            popupEnabled: false,
+            queryable: true,
+            popupFields: {
+                'Name': { field: 'fullname', type: 'string', transform: (value) => toTitleCase(value || '') },
+            },
+        },
+    ],
+};
+
 // Seamless Geological Units WMS Layer
 const seamlessGeolunitsLayerName = 'seamlessgeolunits';
 const seamlessGeolunitsWMSTitle = 'Geological Units (500k)';
@@ -636,7 +656,8 @@ const infrastructureAndLandUseConfig: LayerProps = {
         geothermalPowerplantsWMSConfig,
         pipelinesWMSConfig,
         riversWMSConfig,
-        SITLAConfig
+        SITLAConfig,
+        roadsWMSConfig,
     ]
 }
 
