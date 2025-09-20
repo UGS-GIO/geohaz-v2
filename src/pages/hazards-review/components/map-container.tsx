490c0541698444c3bd3e376b473a90d4548a8b1f
Clinton Lunn <clunn@utah.gov>
2025-09-19 22:53:32 -0600

    refactor(common): remove empty useeffect

--- src/pages/hazards-review/components/map-container.tsx
+++ src/pages/hazards-review/components/map-container.tsx
@@ -4,16 +4,20 @@ import { PopupDrawer } from "@/components/custom/popups/popup-drawer";
 import { useMapContainer } from "@/hooks/use-map-container";
 import { PROD_GEOSERVER_URL } from '@/lib/constants';
 import { HazardsReviewSearchParams } from '@/routes/hazards-review';
-import { useEffect } from 'react';
 import { useGetLayerConfigs } from '@/hooks/use-get-layer-configs';
 
 
 interface MapContainerProps {
-    searchParams: HazardsReviewSearchParams;
-    updateLayerSelection: (layerTitle: string, selected: boolean) => void;
+    searchParams?: HazardsReviewSearchParams;
+    updateLayerSelection?: (layerTitle: string, selected: boolean) => void;
 }
 
 export default function MapContainer({ searchParams, updateLayerSelection }: MapContainerProps) {
+    console.log({
+        searchParams,
+        updateLayerSelection
+    });
+
     const layersConfig = useGetLayerConfigs();
 
     const {
@@ -27,19 +31,10 @@ export default function MapContainer({ searchParams, updateLayerSelection }: Map
         handleOnContextMenu,
         coordinates,
         setCoordinates,
-        view,
     } = useMapContainer({
         wmsUrl: `${PROD_GEOSERVER_URL}wms`,
         layersConfig: layersConfig
     });
-
-
-    // hazards-review-specific effect for applying filters on initial load
-    useEffect(() => {
-        // Wait for the map and filters to be ready
-        if (!view || !view.map) return;
-    }, [view, searchParams.filters, updateLayerSelection]);
-
 
     return (
         <>
