56579911d83c301db1afca23d254b21d75bb3a5e
Clinton Lunn <clunn@utah.gov>
2025-09-22 09:45:13 -0600

    refactor(common): loading spinner to skeleton

--- src/components/sidebar/sidebar-skeleton.tsx
+++ src/components/sidebar/sidebar-skeleton.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
+import { LoadingSpinner } from '../custom/loading-spinner';
 
 interface NavSkeletonProps {
     className?: string;
@@ -17,37 +18,7 @@ export function NavSkeleton({ className }: NavSkeletonProps) {
 
             {/* Skeleton for the Right Content/Links Area */}
             <div className='w-full p-4 md:py-2 md:px-6'>
-                <div className='grid gap-5'>
-                    {/* Simulate two standard nav links */}
-                    {[...Array(2)].map((_, i) => (
-                        <div key={i} className='flex items-center gap-3'>
-                            <Skeleton className='h-6 w-6 shrink-0 rounded-sm' />
-                            <Skeleton className='h-4 w-4/5' />
-                        </div>
-                    ))}
-
-                    {/* Simulate a dropdown/collapsible section */}
-                    <div className='space-y-3 pt-2'>
-                        {/* Trigger */}
-                        <div className='flex items-center gap-3'>
-                            <Skeleton className='h-6 w-6 shrink-0 rounded-sm' />
-                            <Skeleton className='h-4 w-full' />
-                        </div>
-                        {/* Indented sub-links */}
-                        <div className='pl-9 space-y-3'>
-                            <Skeleton className='h-3 w-11/12' />
-                            <Skeleton className='h-3 w-4/5' />
-                        </div>
-                    </div>
-
-                    {/* Simulate two more standard links */}
-                    {[...Array(2)].map((_, i) => (
-                        <div key={i} className='flex items-center gap-3 pt-2'>
-                            <Skeleton className='h-6 w-6 shrink-0 rounded-sm' />
-                            <Skeleton className='h-4 w-3/4' />
-                        </div>
-                    ))}
-                </div>
+                <LoadingSpinner className='my-20 mx-auto' />
             </div>
         </div>
     );
