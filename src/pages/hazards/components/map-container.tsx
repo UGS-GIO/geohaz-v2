f7b815bc1c8cd62a8749d1a831b68064b2de35dd
Clinton Lunn <clunn@utah.gov>
2025-09-23 14:34:58 -0600

    refactor(hazards): add to hazards

--- src/pages/hazards/components/map-container.tsx
+++ src/pages/hazards/components/map-container.tsx
@@ -4,6 +4,9 @@ import { PopupDrawer } from "@/components/custom/popups/popup-drawer";
 import { useMapContainer } from "@/hooks/use-map-container";
 import { PROD_GEOSERVER_URL } from '@/lib/constants';
 import { useGetLayerConfigsData } from '@/hooks/use-get-layer-configs';
+import { LoadingSpinner } from '@/components/custom/loading-spinner';
+import { useIsMapLoading } from '@/hooks/use-is-map-loading';
+import { useMap } from '@/hooks/use-map';
 
 export default function MapContainer() {
     const defaultLayersConfig = useGetLayerConfigsData('layers');
@@ -23,6 +26,10 @@ export default function MapContainer() {
         wmsUrl: `${PROD_GEOSERVER_URL}wms`,
         layersConfig: defaultLayersConfig
     });
+    const { view } = useMap();
+    const isMapLoading = useIsMapLoading({
+        view: view,
+    });
 
     return (
         <>
@@ -34,6 +41,11 @@ export default function MapContainer() {
                 {...clickOrDragHandlers}
             >
                 <MapWidgets />
+                {isMapLoading && (
+                    <div className="absolute inset-0 flex items-center justify-center z-50 bg-gray-50 bg-opacity-75">
+                        <LoadingSpinner />
+                    </div>
+                )}
             </div>
             <PopupDrawer
                 container={popupContainer}
